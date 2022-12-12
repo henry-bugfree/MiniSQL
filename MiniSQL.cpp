58a326f61ebce972ee3ebab0d06ed28e5d09af13
henry-bugfree <Henryliang2@yeah.net>
2022-12-12 16:47:13 +0800

    Update MiniSQL.cpp

--- MiniSQL.cpp
+++ MiniSQL.cpp
@@ -8,52 +8,50 @@
 #include <sys/stat.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <map>
+#include <unistd.h>
 
 using namespace std;
 
-struct Tables {
-	string name;
-	string path;
-	vector<string>colName;//列名称
+string data_path = "./data";
+
+struct Table {
+	string tb_name;
+	string tb_path;
+	vector<string>colName;
 	vector<string>type;
-	vector<int>size;
-	FILE* fp;
+	string primary;
 };
 
 class Database {
-    bool is_open;
-	vector<Tables*> tables;
 public:
-	string cmd;
-	string path;
+	string db_name;
+	string db_path;
+	map<string, Table*> tables;
 
 	Database() {
-		cmd = "";
-		path = "";
-		is_open = false;
+		db_name = "";
+		db_path = "";
 	}
 	~Database() {
-        cmd = "";
-		path = "";
-        is_open = false;
-		for (auto& tab : tables) {
-			if (tab->fp != NULL)
-				fclose(tab->fp);
-			delete tab;
+		for (auto iter = tables.begin(); iter != tables.end(); iter++) {
+			delete iter->second;
 		}
-		tables.resize(0);
 	}
     void CreateDataBase(string);
     void DropDataBase(string);
-    void CreateTable(string);
+    void CreateTable(string, Table*);
     void DropTable(string);
 };
 
 void Database::CreateDataBase(string name){
-    string path = "./" + name;
+    string path = data_path + "/" + name;
     if(opendir(path.c_str())==nullptr){
-        if (mkdir(name.c_str(),0755)==0)
-            cout << "database created!" << endl;
+        if (mkdir(path.c_str(),0755)==0){
+			db_path = path;
+			db_name = name;
+			cout << "database created!" << endl;
+		}
         else
             cout << "database fail to create!" << endl;
     }
@@ -62,11 +60,12 @@ void Database::CreateDataBase(string name){
 }
 
 void Database::DropDataBase(string name){
-    string path = "./" + name;
+    string path = data_path + "/" + name;
     string command = "rm -r " + path;
     if(opendir(path.c_str())!=nullptr){
-        if (system(command.c_str())==0)
-            cout << name << " dropped" << endl;
+        if (system(command.c_str())==0){
+			cout << name << " dropped" << endl;
+		}
         else
             cout << "fail to drop database" << name << endl;
     }
@@ -74,35 +73,150 @@ void Database::DropDataBase(string name){
     return;
 }
 
-void Database::CreateTable(string name){
-    return;
+// create table person (id int primary, name string)
+void Database::CreateTable(string name, Table* tb){
+	string tmp;
+	while(1){
+		cin >> tmp;
+		if(tmp[0]=='('){
+			tmp.erase(tmp.begin());
+			tb->colName.push_back(tmp);
+		}
+		else{
+			tb->colName.push_back(tmp);
+		}
+		cin >> tmp;
+		auto back = tmp.back();
+		if(back==','){
+			tmp.pop_back();
+			tb->type.push_back(tmp);
+			continue;
+		}
+		else if(back==')'){
+			tmp.pop_back();
+			tb->type.push_back(tmp);
+			break;
+		}
+		else{
+			tb->type.push_back(tmp);
+			cin >> tmp;
+			back = tmp.back();
+			tmp.pop_back();
+			if(back==',' && tmp == "primary"){
+				tb->primary=tb->colName.back();
+				continue;
+			}
+			else if(back==')' && tmp == "primary"){
+				tb->primary=tb->colName.back();
+				break;
+			}
+			else{
+				cout << "error in syntax when creating table!" << endl;
+				break;				
+			}		
+		}
+	}
+	tb->tb_name=name;
+	tb->tb_path = data_path + "/" + db_name + "/" + name + ".dat";
+	ofstream outFile;
+	outFile.open(tb->tb_path);
+	outFile << tb->tb_name << endl;
+	outFile << tb->tb_path << endl;
+	outFile << tb->primary << endl;
+	for(auto& col : tb->colName){
+		outFile << col << " ";
+	}
+	outFile << endl;
+	for(auto& tp : tb->type){
+		outFile << tp << " ";
+	}
+	outFile << endl;
+	outFile.close();
+	cout << "table created!" << endl;
+	return;
 }
 
 void Database::DropTable(string name){
-    return;
+	string path = db_path + "/" + name + ".dat";
+    string command = "rm " + path;
+    if(access(path.c_str(), F_OK) == 0){
+        if (system(command.c_str()) == 0){
+			cout << name << " dropped" << endl;
+		}
+        else
+            cout << "fail to drop table" << name << endl;
+    }
+    else cout << "table does not exist!" << endl;
+	return;
 }
 
+Database* current_db;
+map<string, Database*> dbs;
+
 int main() {
-	Database db;
-	while (cin >> db.cmd) {
-		if (db.cmd == "exit") 
-            break;
-		else if (db.cmd == "create") {
-			string name;
-			cin >> db.cmd >> name;
-			if (db.cmd == "database")
-				db.CreateDataBase(name);
-			else if (db.cmd == "table")
-				db.CreateTable(name);
+	string cmd;
+	string name;
+	while (cin >> cmd) {
+		if (cmd == "exit"){
+			for (auto iter = dbs.begin(); iter != dbs.end(); iter++) {
+				delete iter->second;
+			}
+			break;
+		} 
+		else if (cmd == "use"){
+			cin >> name;
+			if(dbs.count(name) == 0){
+				cout << "database does not exist!" << endl;
+				break;
+			}
+			else{
+				current_db = dbs[name];
+				cout << name << " selected!" << endl;
+			}
+		}
+		else if (cmd == "create") {
+			cin >> cmd >> name;
+			if (cmd == "database"){
+				auto db_ptr = new Database();
+				db_ptr->CreateDataBase(name);
+				dbs[name] = db_ptr;
+			}
+			else if (cmd == "table"){
+				if(current_db == nullptr){
+					cout << "no database selected!" << endl;
+					continue;
+				}
+				auto tb = new Table();
+				current_db->CreateTable(name, tb);
+				current_db->tables[name] = tb;
+			}
 			else cout << "errors in create command!" << endl;
 		}
-        else if (db.cmd == "drop") {
-			string name;
-			cin >> db.cmd >> name;
-			if (db.cmd == "database")
-				db.DropDataBase(name);
-			else if (db.cmd == "table")
-				db.DropTable(name);
+        else if (cmd == "drop") {
+			cin >> cmd >> name;
+			if (cmd == "database"){
+				if(dbs.count(name) == 0){
+					cout << "database does not exist!" << endl;
+					break;
+				}
+				else{
+					auto drop_db = dbs[name];
+					if(current_db == drop_db)
+						current_db = nullptr;
+					drop_db->DropDataBase(name);
+					auto iter = dbs.find(name);
+					dbs.erase(iter);
+				}
+			}
+			else if (cmd == "table"){
+				if(current_db == nullptr){
+					cout << "no database selected!" << endl;
+					continue;
+				}
+				current_db->DropTable(name);
+				auto iter = current_db->tables.find(name);
+				current_db->tables.erase(iter);
+			}
 			else cout << "errors in drop command!" << endl;
 		}
 		else {
@@ -114,5 +228,7 @@ int main() {
 	return 0;
 }
 
+// Study/Modern\ C++\ Programming/MiniSQL/
 // create database test
 // drop database test
+// create table person (id int primary, name string)
